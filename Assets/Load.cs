248ff4dc24377162acd79cf920caf29a964410f0
GameDev <dkazmi.bese17seecs@seecs.edu.pk>
2019-07-30 02:46:34 +0500

    Day 1

---
+++ Assets/Load.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Load : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("UpdateP", 5f);
+    }
+
+    // Update is called once per frame
+    void UpdateP()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
