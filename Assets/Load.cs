2f329fa667a3a68f49e01306b9482f8d7426f61a
GameDev <dkazmi.bese17seecs@seecs.edu.pk>
2019-07-31 02:57:51 +0500

    Day 2

--- Assets/Load.cs
+++ Assets/Load.cs
@@ -6,8 +6,14 @@ using UnityEngine.SceneManagement;
 
 public class Load : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void Awake()
+    {
+        
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+// Start is called before the first frame update
+void Start()
     {
         Invoke("UpdateP", 5f);
     }
