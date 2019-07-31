97eaa4216108e91d94bcd58f85d2238aa840bb7f
GameDev <dkazmi.bese17seecs@seecs.edu.pk>
2019-08-01 02:16:08 +0500

    Day3 till video 18

--- Assets/ship.cs
+++ Assets/ship.cs
@@ -1,9 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class ship : MonoBehaviour
 {
+    [SerializeField] float xmov = 20f;
+    [SerializeField] float xspeed = 17f;
+
+    [SerializeField] float ymov = 17f;
+    [SerializeField] float yspeed = 12f;
+
+    [SerializeField] float controlpitch = 10f;
+    [SerializeField] float controlroll = 14f;
+
+    private float xThrow;
+    private float yThrow;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +26,31 @@ public class ship : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        xMovement();
+
+        yMovement();
+
+        float pitch = transform.localPosition.y + yThrow*controlpitch;
+        float yaw= transform.localPosition.x ;
+        float roll = xThrow * controlroll;
+        transform.localRotation = Quaternion.Euler(-pitch,yaw,roll);
+    }
+
+    private void yMovement()
+    {
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float yoffset = yspeed * yThrow * Time.deltaTime;
+        float ynew = transform.localPosition.y + yoffset;
+        float yrnge = Mathf.Clamp(ynew, -ymov, ymov);
+        transform.localPosition = new Vector3(transform.localPosition.x, yrnge, transform.localPosition.z);
+    }
+
+    private void xMovement()
+    {
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xoffset = xspeed * xThrow * Time.deltaTime;
+        float xnew = transform.localPosition.x + xoffset;
+        float xrnge = Mathf.Clamp(xnew, -xmov, xmov);
+        transform.localPosition = new Vector3(xrnge, transform.localPosition.y, transform.localPosition.z);
     }
 }
