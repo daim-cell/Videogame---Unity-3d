ac51c3f7f14ccd312fa2a960a1f143309a376a08
GameDev <dkazmi.bese17seecs@seecs.edu.pk>
2019-08-08 02:46:06 +0500

    Till video 43

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,10 +7,11 @@ public class Enemy : MonoBehaviour
     [SerializeField] GameObject Explosion;
     Scoreboard scoreboard;
     [SerializeField] int shipscore=10;
+    [SerializeField] int hit = 10;
 
     private void Death()
     {
-        scoreboard.Hit(shipscore);
+        Instantiate(Explosion, transform.position, Quaternion.identity);
         Destroy(this.gameObject);
     }
     // Start is called before the first frame update
@@ -21,9 +22,12 @@ public class Enemy : MonoBehaviour
 
     void OnParticleCollision(GameObject other)
     {
-        Instantiate(Explosion, transform.position, Quaternion.identity);
-
-        Death();
+        scoreboard.Hit(shipscore);
+        hit-=1;
+        if (hit < 0)
+        {
+            Death();
+        }
     }
     // Update is called once per frame
     void Update()
